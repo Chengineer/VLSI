d1f1a4b1178740ece76e14d4e5f9c2ddb9490ce5
Chen Grapel <36038782+Chengineer@users.noreply.github.com>
2021-09-14 14:14:42 +0300

    Add files via upload

---
+++ Design and Verification Training Program/Veriest/SystemVerilog/Cadence SystemVerilog for Design and Verification v20.6/lab16-alucov/files/xcelium.d/run.lnx8664.19.09.d/files.ts
@@ -0,0 +1,3 @@
+1518181990 /home/cheng/Desktop/svlabs/lab16-alucov/files/typedefs.sv
+1518181990 /home/cheng/Desktop/svlabs/lab16-alucov/files/alu.sv
+1629874669 /home/cheng/Desktop/svlabs/lab16-alucov/files/alu_test.sv
