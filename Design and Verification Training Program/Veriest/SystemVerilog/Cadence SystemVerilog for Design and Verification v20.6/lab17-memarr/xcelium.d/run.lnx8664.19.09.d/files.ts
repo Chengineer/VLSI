26e040fc1cae0e81e539b9e652c2cb2cfca0df13
Chen Grapel <36038782+Chengineer@users.noreply.github.com>
2021-09-14 14:19:14 +0300

    Add files via upload

---
+++ Design and Verification Training Program/Veriest/SystemVerilog/Cadence SystemVerilog for Design and Verification v20.6/lab17-memarr/xcelium.d/run.lnx8664.19.09.d/files.ts
@@ -0,0 +1,5 @@
+1625493657 /home/cheng/Desktop/svlabs/lab17-memarr/mem.sv
+1625492903 /home/cheng/Desktop/svlabs/lab17-memarr/top.sv
+1626074284 /home/cheng/Desktop/svlabs/lab17-memarr/mem_interface.sv
+1629882717 /home/cheng/Desktop/svlabs/lab17-memarr/mem_test_associative_array_scoreboard.sv
+1629888009 /home/cheng/Desktop/svlabs/lab17-memarr/mem_test_queue_scoreboard.sv
