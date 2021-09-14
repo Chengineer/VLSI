fab712e217cf2595f79e17d2d2a132ad6e6aaa02
Chen Grapel <36038782+Chengineer@users.noreply.github.com>
2021-09-14 13:39:36 +0300

    Add files via upload

---
+++ Design and Verification Training Program/Veriest/SystemVerilog/Cadence SystemVerilog for Design and Verification v20.6/lab05-alu/typedefs.sv
@@ -0,0 +1,6 @@
+package typedefs;
+   
+   typedef enum logic [2:0] {HLT, SKZ, ADD, AND, XOR, LDA, STO, JMP} opcode_t;
+   typedef enum logic [2:0] {INST_ADDR, INST_FETCH, INST_LOAD, IDLE, OP_ADDR, OP_FETCH, ALU_OP, STORE} state_t;
+
+endpackage : typedefs
---
+++ Design and Verification Training Program/Veriest/SystemVerilog/Cadence SystemVerilog for Design and Verification v20.6/lab05-alu/xcelium.d/run.lnx8664.19.09.d/files.ts
@@ -0,0 +1,3 @@
+1624535643 /home/cheng/Desktop/svlabs/lab05-alu/typedefs.sv
+1624956001 /home/cheng/Desktop/svlabs/lab05-alu/alu.sv
+1518181990 /home/cheng/Desktop/svlabs/lab05-alu/alu_test.sv
