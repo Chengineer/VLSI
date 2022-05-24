f11afba3ed6a040e66d0509bab017f4adcad1d4f
Chen Grapel <36038782+Chengineer@users.noreply.github.com>
2022-05-24 16:15:54 +0300

    Add files via upload

---
+++ Design and Verification Training Program/Veriest/UVM/Cadence SystemVerilog Accelerated Verification Usign UVM (Engineer Explorer Series) v1.2.5/lab09_sb/lab09_sba/tb/router_tb.sv
@@ -0,0 +1,80 @@
+class router_tb extends uvm_env;
+
+	// component macro
+	`uvm_component_utils(router_tb)
+
+	// YAPP UVC class handle
+	yapp_env env;
+
+	// Channel UVC handles
+	channel_env ch0;
+	channel_env ch1;
+	channel_env ch2;
+
+	// HBUS UVC handle
+	hbus_env hbus;
+
+	// Clock and Reset UVC handle
+	clock_and_reset_env clock_and_reset;
+
+	// multichannel (virtual) sequencer handle
+	router_mcsequencer mcseqr;
+
+	// scoreboard declaration
+	router_scoreboard router_sb;
+	
+	// component constructor
+	function new(string name, uvm_component parent);
+		super.new(name, parent);
+	endfunction
+	
+	// UVM build_phase
+	virtual function void build_phase(uvm_phase phase);
+		super.build_phase(phase);
+		`uvm_info("MSG", "Testbench build_phase executed", UVM_HIGH)
+		//env = new("env", this);
+		env = yapp_env::type_id::create("env", this); // factory create call for instances
+		// configure the UVC before creating for safety.
+		// Channel UVC
+		// set channel_id property of each channel
+		uvm_config_int::set(this, "ch0", "channel_id", 0);
+		uvm_config_int::set(this, "ch1", "channel_id", 1);
+		uvm_config_int::set(this, "ch2", "channel_id", 2);
+		// create instances using factory calls
+		ch0 = channel_env::type_id::create("ch0", this);
+		ch1 = channel_env::type_id::create("ch1", this);
+		ch2 = channel_env::type_id::create("ch2", this);
+		// HBUS UVC - 1 master and 0 slaves
+		// The HBUS UVC has both master and slave agents. For the router testing, we only need the master agent.
+		uvm_config_int::set(this, "hbus", "num_masters", 1);
+		uvm_config_int::set(this, "hbus", "num_slaves", 0);
+		// create instance using factory calls
+		hbus = hbus_env::type_id::create("hbus", this);
+		// Clock and Reset UVC - requires no configuration
+		// create instance using factory calls 
+		clock_and_reset = clock_and_reset_env::type_id::create("clock_and_reset", this);
+		// virtual sequencer instance (handle and instance names match, so we can read the pathname off a topology report)
+		mcseqr = router_mcsequencer::type_id::create("mcseqr", this);
+		// create scoreboard
+		router_sb = router_scoreboard::type_id::create("router_sb", this);
+		
+	endfunction
+
+	virtual function void connect_phase(uvm_phase phase);
+		// virtual sequencer connections (connect its UVC sequencer handles to the actual UVC sequencer instances via hierarchial pathnames. these pathnames are not string, se we can't use wildcards, and they are also handle names, not instance names)
+		mcseqr.hbus_seqr = hbus.masters[0].sequencer;
+		mcseqr.yapp_seqr = env.tx_agent.sequencer;
+		// connect the TLM ports from the YAPP and Channel UVCs to the scoreboard
+		// connect the YAPP and Channel monitor ports to the scoreboard imps using hierarchical pathnames. 
+		// REMEMBER an unconnected analysis port is not an error, so make sure you connect everything
+		env.tx_agent.monitor.item_collected_port.connect(router_sb.sb_yapp_in);
+		ch0.rx_agent.monitor.item_collected_port.connect(router_sb.sb_chan0);
+		ch1.rx_agent.monitor.item_collected_port.connect(router_sb.sb_chan1);
+		ch2.rx_agent.monitor.item_collected_port.connect(router_sb.sb_chan2);
+	endfunction
+
+	function void start_of_simulation_phase(uvm_phase phase);
+		`uvm_info(get_type_name(), {"start of simulation for:", get_full_name()}, UVM_HIGH)
+	endfunction
+	
+endclass
---
+++ Design and Verification Training Program/Veriest/UVM/Cadence SystemVerilog Accelerated Verification Usign UVM (Engineer Explorer Series) v1.2.5/lab09_sb/lab09_sba/tb/tb_top.sv
@@ -0,0 +1,50 @@
+/*-----------------------------------------------------------------
+File name     : tb_top.sv
+Description   : lab01_data top module template file
+Notes         : From the Cadence "SystemVerilog Advanced Verification with UVM" training
+-------------------------------------------------------------------
+Copyright Cadence Design Systems (c)2015
+-----------------------------------------------------------------*/
+
+module tb_top;
+// import the UVM library
+	import uvm_pkg::*;
+// include the UVM macros
+	`include "uvm_macros.svh"
+// we import UVC packages before the testbench include, as the testbench uses the UVC package declarations.
+// import the YAPP package
+	import yapp_pkg::*;
+// import the Channel UVC package
+	import channel_pkg::*;
+// import the clock and reset UVC package
+	import clock_and_reset_pkg::*;
+// import the HBUS UVC package
+	import hbus_pkg::*;
+// include the multichannel sequencer
+	`include "router_mcsequencer.sv"
+// include the multichannel sequencer sequences
+	`include "router_mcseqs_lib.sv"
+// ^these 2 are included before the tb which makes the instantiation
+
+// include the scoreboard file
+	`include "router_scoreboard.sv"
+// include the testbench and test library file
+	`include "router_tb.sv"
+	`include "router_test_lib.sv"
+
+
+	initial begin
+		yapp_vif_config::set(null,"uvm_test_top.tb.env.tx_agent.*","vif",hw_top.in0); // the interface has to be set in advance of building the UVC
+		hbus_vif_config::set(null, "*.tb.hbus.*", "vif", hw_top.hif);
+		// set the HBUS, Channel and clock and reset UVC virtual interfaces to the correct interface. (the UVC header files contain typedefs for each interface). we need a set call for each UVC instance to connect the interfaces. pathnames contain the UVC instance names we used in the testbench, and then a wildcard * to hit everything under the UVC top level instance.
+		// since we created the YAPP, we know the interfaces are only used in driver and monitor under the agent. however third-party UVCs may, rarely, access the interface in other parts of the hierarchy, so it's safer just to select the top level instance.
+		// the field name should match that used in the get. all our UVCs use vif as the field name, but in reality, you should always confirm which field name is used. ideally this is documented, but we may need to open the driver and monitor code to check.
+		// the interface instance names we used in the hw_top module are accessed via a hierarchical pathname.
+		channel_vif_config::set(null, "*.tb.ch0.*", "vif", hw_top.ch0if);
+		channel_vif_config::set(null, "*.tb.ch1.*", "vif", hw_top.ch1if);
+		channel_vif_config::set(null, "*.tb.ch2.*", "vif", hw_top.ch2if);
+		clock_and_reset_vif_config::set(null, "*.tb.clock_and_reset.*", "vif", hw_top.clk_rst_if);
+		run_test("base_test"); // initiate phasing
+	end
+	
+endmodule : tb_top
