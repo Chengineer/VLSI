4a4c03a99e4dd3a2e1f2d7bbea319d67ad3590f6
Chen Grapel <36038782+Chengineer@users.noreply.github.com>
2022-05-24 16:08:17 +0300

    Add files via upload

---
+++ Design and Verification Training Program/Veriest/UVM/Cadence SystemVerilog Accelerated Verification Usign UVM (Engineer Explorer Series) v1.2.5/lab06_vif/sv/yapp_pkg.sv~
@@ -0,0 +1,16 @@
+package yapp_pkg;
+
+	import uvm_pkg::*;
+
+	typedef uvm_config_db #(virtual interface yapp_if) yapp_vif_config; // convenience type declaration
+
+	`include "uvm_macros.svh"
+	`include "yapp_packet.sv"
+	`include "yapp_tx_monitor.sv"
+	`include "yapp_tx_sequencer.sv"
+	`include "yapp_tx_seqs.sv"
+	`include "yapp_tx_driver.sv"
+	`include "yapp_tx_agent.sv"
+	`include "yapp_env.sv"
+
+endpackage
---
+++ Design and Verification Training Program/Veriest/UVM/Cadence SystemVerilog Accelerated Verification Usign UVM (Engineer Explorer Series) v1.2.5/lab06_vif/sv/yapp_tx_driver.sv
@@ -0,0 +1,76 @@
+class yapp_tx_driver extends uvm_driver #(yapp_packet);
+	
+	// component utility macro
+	`uvm_component_utils(yapp_tx_driver) 
+	
+	// virtual interface declaration
+	virtual interface yapp_if vif;
+
+	// Declare this property to count packets sent
+        int num_sent;
+
+	// component constructor - required syntax for UVM automation and utilities
+	function new(string name, uvm_component parent); 
+		super.new(name, parent);
+	endfunction
+
+  // UVM run_phase
+  task run_phase(uvm_phase phase);
+    fork
+      get_and_drive();
+      reset_signals();
+    join
+  endtask : run_phase
+
+  // Gets packets from the sequencer and passes them to the driver. 
+  task get_and_drive();
+    @(posedge vif.reset);
+    @(negedge vif.reset);
+    `uvm_info(get_type_name(), "Reset dropped", UVM_MEDIUM)
+    forever begin
+      // Get new item from the sequencer
+      seq_item_port.get_next_item(req);
+
+      `uvm_info(get_type_name(), $sformatf("Sending Packet :\n%s", req.sprint()), UVM_HIGH)
+       
+      // concurrent blocks for packet driving and transaction recording
+      fork
+        // send packet
+        begin
+          // for acceleration efficiency, write unsynthesizable dynamic payload array directly into 
+          // interface static payload array
+          foreach (req.payload[i])
+            vif.payload_mem[i] = req.payload[i];
+          // send rest of YAPP packet via individual arguments
+          vif.send_to_dut(req.length, req.addr, req.parity, req.packet_delay);
+        end
+        // trigger transaction at start of packet (trigger signal from interface)
+        @(posedge vif.drvstart) void'(begin_tr(req, "Driver_YAPP_Packet"));
+      join
+
+      // End transaction recording
+      end_tr(req);
+      num_sent++;
+      // Communicate item done to the sequencer
+      seq_item_port.item_done();
+    end
+  endtask : get_and_drive
+
+  // Reset all TX signals
+  task reset_signals();
+    forever 
+     vif.yapp_reset();
+  endtask : reset_signals
+
+  // UVM report_phase
+  function void report_phase(uvm_phase phase);
+    `uvm_info(get_type_name(), $sformatf("Report: YAPP TX driver sent %0d packets", num_sent), UVM_LOW)
+  endfunction : report_phase
+
+  // connect phase
+  function void connect_phase(uvm_phase phase);
+    if(!yapp_vif_config::get(this, "", "vif", vif))
+      `uvm_error("NOVIF", {"Virtual interface must be set for: ", get_full_name(), ".vif"})
+  endfunction : connect_phase
+
+endclass: yapp_tx_driver
